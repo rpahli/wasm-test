7ae9c8bcf65ba0ef7d0baa9d8aa2b0d7a109c440
Rico Pahlisch <rico.pahlisch@grayc.de>
2024-09-11 14:57:01 +0200

    initial commit

---
+++ assembly/index.ts
@@ -0,0 +1,31 @@
+export * from "@kong/proxy-wasm-sdk/assembly/proxy";
+import {
+  RootContext,
+  Context,
+  registerRootContext,
+  FilterHeadersStatusValues,
+  stream_context
+} from "@kong/proxy-wasm-sdk/assembly";
+
+class AddHeaderRoot extends RootContext {
+  createContext(context_id: u32): Context {
+    return new AddHeader(context_id, this);
+  }
+}
+
+class AddHeader extends Context {
+  constructor(context_id: u32, root_context: AddHeaderRoot) {
+    super(context_id, root_context);
+  }
+  onResponseHeaders(a: u32, end_of_stream: bool): FilterHeadersStatusValues {
+    const root_context = this.root_context;
+    if (root_context.getConfiguration() == "") {
+      stream_context.headers.response.add("hello", "world!");
+    } else {
+      stream_context.headers.response.add("hello", root_context.getConfiguration());
+    }
+    return FilterHeadersStatusValues.Continue;
+  }
+}
+
+registerRootContext((context_id: u32) => { return new AddHeaderRoot(context_id); }, "add_header");
